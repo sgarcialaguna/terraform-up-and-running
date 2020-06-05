096f7b6e9926b55143dbab1db32fea1cffcc6dbe
Santiago Garcia <santiago.garcia@posteo.de>
2020-06-05 19:38:32 +0200

    Simple webserver

--- chapter1/main.tf
+++ chapter1/main.tf
@@ -3,11 +3,27 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  user_data              = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y busybox
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
+  vpc_security_group_ids = [aws_security_group.instance.id]
   tags = {
     Name = "terraform-example"
   }
 }
 
-
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
