ed8f39e919244a1cf81822f5df1dea8fd621edaf
Santiago Garcia <santiago.garcia@posteo.de>
2020-06-06 10:57:15 +0200

    Input and output variables

--- chapter1/main.tf
+++ chapter1/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_instance" "example" {
   ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
@@ -10,7 +16,7 @@ resource "aws_instance" "example" {
     apt-get update
     apt-get install -y busybox
     echo "Hello, World" > index.html
-    nohup busybox httpd -f -p 8080 &
+    nohup busybox httpd -f -p ${var.server_port} &
   EOF
   vpc_security_group_ids = [aws_security_group.instance.id]
   tags = {
@@ -21,9 +27,13 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_dns" {
+  value = aws_instance.example.public_dns
 }
